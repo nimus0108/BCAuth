e3ee2d23af1b79abdf4a75242bd7015d8852cb7a
Robert Kim <rkty13@gmail.com>
2015-05-11 22:03:37 -0400

    sigh

--- test.js
+++ test.js
@@ -11,46 +11,8 @@ var port = process.env.PORT || 3000;
 
 var router = express.Router();
 
-router.use(function(req, res, next) {
-    try {
-        console.log("Request received.");
-        console.dir(req.body);
-        var body = JSON.parse(JSON.stringify(req.body));
-        var username = body.username;
-        var password = body.password;
-        if (Object.keys(body).length > 2) {
-            // Too many key/values!
-            console.log("Request sent back with status: 2");
-            res.json({ 'status': 2 });
-        } else if (!username || !password) {
-            // Username or password is missing.
-            console.log("Request sent back with status: 3");
-            res.json({ 'status': 3 });
-        } else {
-            console.log("Next route.");
-            next();
-        }
-    } catch (e) {
-        // Invalid JSON.
-        console.log("Request sent back with status: 1");
-        res.json({ 'status': 1 });
-    }
-});
-
-router.route('/login').post(function(req, res) {
-    console.log('hi');
-    var username = req.body.username;
-    var password = req.body.password;
-    var browser = new Browser();
-    browser.visit('https://ps01.bergen.org/public', function(err) {
-        if (err) {
-            console.log("Request sent back with status: 4");
-            // Error connecting to PowerSchool.
-            res.json({ 'status': 4 })
-        } else {
-            login(browser, username, password, res);
-        }
-    });
+router.route('/login').get(function(req, res) {
+    res.json({"hi":"asdf"});
 });
 
 function login(browser, username, password, res) {
